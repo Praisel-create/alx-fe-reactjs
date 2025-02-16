de95c2334f41b40366ca90aedd9cc79f553e3706
Praisel <praiseozoko3@gmail.com>
2025-02-16 16:04:43 +0100

    Formatting App.jsx

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -3,13 +3,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from "./src/Header";
-import MainContent from "./src/MainContent";
-import Footer from "./src/Footer";
+import Header from '../Header'
+import MainContent from './MainContent'
+import Footer from '../Footer'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
