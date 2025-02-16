b0665e8d46eeed47f1e13070badf0901d5ec4cc9
Praisel <praiseozoko3@gmail.com>
2025-02-16 12:39:34 +0100

    Making one App.jsx file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from "./src/Header";
+import MainContent from "./src/MainContent";
+import Footer from "./src/Footer";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +13,9 @@ function App() {
   return (
     <>
       <div>
+        <Header />
+        <MainContent />
+        <Footer />
         <WelcomeMessage />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
