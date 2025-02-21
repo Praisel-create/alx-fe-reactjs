4e409828ceba8b1c0e827fe5ac70c7ab29b43102
Praisel <praiseozoko3@gmail.com>
2025-02-21 11:56:38 +0100

    Adjusted typo

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -4,8 +4,8 @@ function Counter() {
     const [count, setCount] = useState(0);
     return(
         <div>
-        <button onClick={() => setCount(count + 1)}>Increase</button>
-        <button onClick={() => setCount(count - 1)}>Decrease</button>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
         <button onClick={() => setCount(0)}>Reset</button>
         
         <p>Count: {count}</p>
