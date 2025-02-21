9efcd2c3cd879385d968542d5cbb2def4f255477
Praisel <praiseozoko3@gmail.com>
2025-02-21 11:19:09 +0100

    Styling the new react-app file

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {       
+    return (
+        <footer>
+            <p> <span style={{ fontStyle: 'italic', fontSize: '10px' }}>&copy; 2023 City Lovers</span></p>
+        </footer>
+    );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+    return (
+        <main>
+            <p> <span style={{ fontStyle: 'oblique', fontSize: '15px' }}>I love to visit New York, Paris, and Tokyo.</span></p>
+        </main>
+    );
+}
+export default MainContent;
