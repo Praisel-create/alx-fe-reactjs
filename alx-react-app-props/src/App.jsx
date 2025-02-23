34b353022c15b447b8be8a8eb381d6d8f25ef3e1
Praisel <praiseozoko3@gmail.com>
2025-02-23 15:00:21 +0100

    Implemention context

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,26 +7,19 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import UserProfile from './components/UserProfile'
 import Footer from './components/Footer'
+import UserContext from './components/UserContext'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
       <WelcomeMessage />
       <Header />
       <MainContent />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserContext />
     </>
   )
 }
