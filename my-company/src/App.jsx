b5ec37fe3b71a362e360c0197a384616269bd90a
Praisel <praiseozoko3@gmail.com>
2025-02-23 19:25:46 +0100

    correction

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-import Services from './components/Services'
-import Contact from './components/Contact'
-import Navbar from './components/Navbar'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Services from "./Services";
+import Contact from "./Contact";
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <>
-    <Navbar />
     <Router>
-      <Router>
-        <Route path='/' element={<Home />} />
-        <Route path='/' element={<About />} />
-        <Route path='/' element={<Services />} />
-        <Route path='/' element={<Contact />} />
-      </Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to React</h1>
+          <Navbar />
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </Router>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
