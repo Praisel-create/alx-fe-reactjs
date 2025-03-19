ce6fec61a37fd76a1eaa63fe9a85baa5bcfbdc2a
Praisel <praiseozoko3@gmail.com>
2025-03-19 12:34:15 +0100

    Implementing Interactivity within the platform

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
     const [data, setData] = useState([]); //State to store JSON data
 
     useEffect(() => {
-        fetch("/data.json")
+        fetch("./src/data.json")
         .then((response) => response.json())
         .then((jsonData) => setData(jsonData))
         .catch((error) => console.error("Error loading data:", error));
@@ -15,11 +15,11 @@ const HomePage = () => {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
         <h1 className="text-3xl font-bold text-center mb-6">Recipe Collection</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {data.map((item) => (
-                <div key={item.id} className="bg-white p-4 rounded-2xl shadow-lg">
-                    <img src= {item.image} alt= {item.title} className="hover:shadow-lg"/>
-                    <h2 className="text-xl font-semibold mt-3 hover:shadow-lg">{item.title}</h2>
+                <div key={item.id} className="bg-white p-4 rounded-2xl shadow-lg max-w-full">
+                    <img src= {item.image} alt= {item.title} className="h-[200px] w-[200px] hover:shadow-lg "/>
+                    <h2 className="sm:text-sm md:text-l lg:text-xl font-semibold mt-3 hover:shadow-lg">{item.title}</h2>
                     <p className="text-gray-600 mt-1">{item.summary}</p>
                 </div>
             )
