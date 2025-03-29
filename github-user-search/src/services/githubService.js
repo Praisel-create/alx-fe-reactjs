b09ca6ceda9f6336c4f5b8609fee11ce35ec4be7
Praisel <praiseozoko3@gmail.com>
2025-03-29 04:54:09 +0100

    Optimized app and prepared for deployment

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -7,6 +7,7 @@ export const fetchUsers = async (username, location, minRepos, page = 1) => {
       let query = `q=${username}`;
       if (location) query += `+location:${location}`;
       if (minRepos) query += `+repos:>${minRepos}`;
+      
   
       const response = await axios.get(`https://api.github.com/search/users?${query}&per_page=10&page=${page}`);
       return response.data;
