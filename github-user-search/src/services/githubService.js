3860f5a3dcbac4aa3b04b6fd92f542047f5a47f4
Praisel <praiseozoko3@gmail.com>
2025-03-29 04:37:37 +0100

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/search/users";
+const BASE_URL = "https://api.github.com/search/users?q";
 
 export const fetchUsers = async (username, location, minRepos, page = 1) => {
   try {
